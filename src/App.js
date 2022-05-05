b2742e92bdd0c7d3f1379b2caae55674ac58d1c4
Derek Onay <wr3tched@gmail.com>
2022-05-04 21:49:11 -0400

    Latest changes

--- src/App.js
+++ src/App.js
@@ -8,15 +8,22 @@ const BookCard = ({Title, Author, Subtitle, Cover, Status, Description}) => (
       <div className="card-body">
         <h5 className="card-title">{Title}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{Subtitle}</h6>
-        <p className="card-text">{Author.Name/* {
-        Author.forEach(author => {
-          console.log(author);
-          return <p className="card-text">{author}</p>
-        })
-        } */}</p>
+        <p className="card-text">{Author}</p>
         <p className="card-text">
           <small className="text-muted">{Description}</small>
         </p>
+      </div>
+    </div>
+  </div>
+);
+
+const BookCard1 = (books) => (
+  <div className="col-sm-4">
+    <div className="card">
+      <div className="card-body">
+        {console.log(books)}
+        <p>{books.Title}</p>
+        <p className="card-text">{books.Name}</p>
       </div>
     </div>
   </div>
@@ -28,22 +35,39 @@ class App extends Component {
     super(props);
     this.state = {
       books: [],
+      authors: [],
+      theWholeThang: []
     };
   }
 
   componentDidMount() {
-    fetch('https://api.airtable.com/v0/app6fOS902cRMhBSw/Books?api_key=keys1vQrD6Mclwr7S')
-    .then((resp) => resp.json())
-    .then(data => {
-      console.log(data);
-      this.setState({ books: data.records });
-    }).catch(err => {
-      // Error
-      console.log('no return in fetch');
+    Promise.all([
+      fetch('https://api.airtable.com/v0/app6fOS902cRMhBSw/Books?api_key=keys1vQrD6Mclwr7S'),
+      fetch('https://api.airtable.com/v0/app6fOS902cRMhBSw/Author?api_key=keys1vQrD6Mclwr7S'),
+    ])
+    .then(([resp1, resp2]) => { 
+      return Promise.all([
+        resp1.json(), 
+        resp2.json()
+      ]) 
+    })
+    .then(([resp1, resp2]) => {
+      // set state in here
+      this.setState({ 
+        books: resp1.records,
+        authors: resp2.records 
+      });
+    })
+    .catch(err => {
+      console.log('no return in fetch' + err);
     });
+
+    
   }
 
   render() {
+    // Trying to combine Authors & Books
+    this.state.theWholeThang = this.state.books.map((el, i) => ({ book: el, authors: this.state.authors[i] }))
     return (
       <div className="container">
         <div className="row">
@@ -56,6 +80,27 @@ class App extends Component {
         </div>
         <div className="row">
           {this.state.books.map(book => <BookCard {...book.fields} /> )}
+        </div>
+        <div className="row">
+          {this.state.theWholeThang.map(book => <BookCard1 {...book.fields} /> )}
+          {/*this.state.authors.map(author => <BookCard1 books={author.Books} authors={author} /> )}
+          {this.state.books.map((val,index) => <BookCard1 data={val} authors={authors[index]} key={index} /> )*/
+          /*
+          const finalData = data.map(dataItem => {
+            dataItem.condition = dataItem.condition ? rulesMap[dataItem.condition] : null;
+            dataItem.helpers.map(item => {
+              item.condition = item.condition ? rulesMap[item.condition] : null
+            })
+            return dataItem;
+          });
+
+          const { data, desc } = this.state;
+          return (
+              <div className="thatThingIAlwaysForgetToAddInReact">
+                data.map((val, index) => <Well data={val} desc={desc[index]} key={index} onDelete={this.onDelete.bind(this)}/>
+              </div>
+            )
+        */}
         </div>
       </div>
     );
